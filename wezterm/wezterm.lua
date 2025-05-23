5b6fa311c266b8d6ae677494108f502770201cfe
MMVS <zeinny1997@gmail.com>
2025-05-23 14:58:37 -0600

    font size change

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,6 +13,6 @@ return {
   color_scheme = "Wez",
 
   -- Additional settings for window appearance
-  window_background_opacity = 0.95,  -- Optional: tweak transparency if you want
+  --  window_background_opacity = 0.95,  -- Optional: tweak transparency if you want
   use_fancy_tab_bar = false,         -- Optional: disable fancy tab bar if you prefer simpler appearance
 }
