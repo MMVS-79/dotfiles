79798900e2c69727b554a01589230ccd27ec65f2
MMVS <zeinny1997@gmail.com>
2025-05-23 14:27:02 -0600

    wezterm conf

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require 'wezterm'
+
+return {
+  -- Window Size
+  initial_cols = 300,
+  initial_rows = 300,
+
+  -- Font Settings
+  font = wezterm.font("Iosevka"),
+  font_size = 16,
+
+  -- Color Scheme (using Wez theme)
+  color_scheme = "Wez",
+
+  -- Additional settings for window appearance
+  window_background_opacity = 0.95,  -- Optional: tweak transparency if you want
+  use_fancy_tab_bar = false,         -- Optional: disable fancy tab bar if you prefer simpler appearance
+}
