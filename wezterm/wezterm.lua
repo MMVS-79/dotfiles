a36a03eded2ef52d062efbfef5e1986630659901
MMVS <zeinny1997@gmail.com>
2025-05-30 20:49:25 -0600

    small wezterm config change

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -15,4 +15,7 @@ return {
   -- Additional settings for window appearance
   --  window_background_opacity = 0.95,  -- Optional: tweak transparency if you want
   use_fancy_tab_bar = false,         -- Optional: disable fancy tab bar if you prefer simpler appearance
+  -- manually setting rendering backend
+  front_end = "WebGpu",
+
 }
