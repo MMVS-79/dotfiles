4344d9f9a34e26ac7effa352bd8003b29de5e94f
MMVS <zeinny1997@gmail.com>
2025-09-18 09:57:14 -0600

    doom changes

--- doom/config.el
+++ doom/config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-xcode)
+(setq doom-theme 'doom-bluloco-dark)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -90,5 +90,5 @@
 ;; Disable auto-save
 (setq auto-save-default nil)
 
-;; Auto kill previous dired buffer
+;; Kill previous dired buffers as you navigate
 (setq dired-kill-when-opening-new-dired-buffer t)
--- doom/custom.el
+++ doom/custom.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0" default)))
+   '("e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0" default))
+ '(package-selected-packages '(gdscript-mode go-mode pdf-tools vterm yaml-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
